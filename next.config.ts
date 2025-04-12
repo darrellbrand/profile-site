c52a0f444f956bee73e5d4c5145b660a9693ab17
dj <darrellbrand@gmail.com>
2025-04-11 22:44:07 -0700

    wiP

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
-
+const repoName = 'profile-site-static';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
