03307da010faedf0c2a090dbf78f0ef0c7bf50a9
dj <darrellbrand@gmail.com>
2025-04-13 16:01:56 -0700

    wip

--- src/components/ui/app-nav-bar.tsx
+++ src/components/ui/app-nav-bar.tsx
@@ -20,23 +20,23 @@ function Navbar({ className }: { className?: string }) {
         >
             <Menu setActive={setActive}>
             <MenuItem setActive={setActive} active={active} item="Home">
-                    <div className="flex flex-col space-y-4 text-sm">
+                    <div className="flex flex-col space-y-10 text-sm">
                         <HoveredLink href="/">Home</HoveredLink>
                     </div>
                 </MenuItem>
                 <MenuItem setActive={setActive} active={active} item="Android">
-                    <div className="flex flex-col space-y-4 text-sm">
+                    <div className="flex flex-col space-y-10 text-sm">
                         <HoveredLink href="/recipe-king">Recipe King</HoveredLink>
                     </div>
                 </MenuItem>
                 <MenuItem setActive={setActive} active={active} item="Web">
-                    <div className="flex flex-col space-y-4 text-sm">
+                    <div className="flex flex-col space-y-10 text-sm">
                         <HoveredLink href="/">Next.js</HoveredLink>
                         <HoveredLink href="/">Spring Boot</HoveredLink>
                     </div>
                 </MenuItem>
                 <MenuItem setActive={setActive} active={active} item="About">
-                    <div className="flex flex-col space-y-4 text-sm">
+                    <div className="flex flex-col space-y-10 text-sm">
                         <HoveredLink href="/about#profile">Profile</HoveredLink>
                         <HoveredLink href="/about#contact">Contact</HoveredLink>
                         <HoveredLink href="https://github.com/darrellbrand">Github</HoveredLink>
